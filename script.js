09a0fc64193976042117f32c88aa15150ff49eb8
shweta-rc13 <shwetachaurasiya1313@gmail.com>
2024-04-06 23:40:08 +0530

    spotify-clone

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 console.log("Let's start Javascript")
 
-let a = fetch(`http://127.0.0.1:5500/songs/`)
+async function main(){
+    let a = await fetch(`http://127.0.0.1:5500/songs/`)
 let request = await a.text();
 console.log(response)
+}
+main()
